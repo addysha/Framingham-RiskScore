57309440f25b8b1d29e95a89f742c8be4535ee76
addysha <addysharmap@gmail.com>
2023-08-28 22:12:46 +1200

    starting start function

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const MIN_AGE = 20;
+const MAX_AGE = 79;
+const MIN_CHOLESTEROL = 0;
+const MAX_CHOLESTEROL = 500;
+const MIN_CHOLESTEROL_HDL = 0;
+const MAX_CHOLESTEROL_HDL = 200;
+const MIN_SYSTOLIC_BLOOD_PRESSURE = 0;
+const MAX_SYSTOLIC_BLOOD_PRESSURE = 300;
+
+var gender;
+var age;
+var cholesterol;
+var cigareteSmoker;
+var cholesterolHDL;
+var systolicBloodPressure;
+var treated;
+var tenYearRisk;
+var points;
+var step;
+
+function start(){
+
+  
+
+
+
+
+}
